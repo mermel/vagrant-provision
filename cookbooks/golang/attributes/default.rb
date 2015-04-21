62cf4ed715c38d433dcb9c4b5231dfc654e5086e
Lauren Mermel <lgmermel@gmail.com>
2015-04-21 09:12:06 -0700

    Revert "updated vagrantfile"

--- cookbooks/golang/attributes/default.rb
+++ cookbooks/golang/attributes/default.rb
@@ -3,8 +3,8 @@ default['go']['platform'] = node['kernel']['machine'] =~ /i.86/ ? '386' : 'amd64
 default['go']['filename'] = "go#{node['go']['version']}.#{node['os']}-#{node['go']['platform']}.tar.gz"
 default['go']['url'] = "http://golang.org/dl/#{node['go']['filename']}"
 default['go']['install_dir'] = '/usr/local'
-default['go']['gopath'] = '/home/vagrant/go'
-default['go']['gobin'] = '/home/vagrant/go/bin'
+default['go']['gopath'] = '/opt/go'
+default['go']['gobin'] = '/opt/go/bin'
 default['go']['scm'] = true
 default['go']['packages'] = []
 default['go']['owner'] = 'root'
